a46476897b74686d219b5c16877b6e84a651d970
jenny han <jlhan@stanford.edu>
2021-07-07 14:03:14 -0700

    reset brush

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+let colorlist = ['green', 'pink', 'turquoise', 'red']
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    background(255);
+    background(0);
 }
 
 function draw() {
-  noStroke();
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  // line(20, 10, mouseX, mouseY);
+  // stroke(random(colorlist));
+  // strokeWieght(1);
+  strokeWeight(1);
+  stroke(random(255), random(200), random(200), 120);
+  line(mouseX, mouseY, random(width), random(height));
+  line(mouseX, mouseY, random(width), random(height));
+  line(mouseX, mouseY, random(width), random(height));
+
+  // gill lines stationary
+  // stroke(0, 0, 0, 10)
+  // line(width / 2, height / 2, random(width), random(height));
+  // strokeWeight(3)
+  // stroke(51, 51, 0, 10)
+  // line(width / 2, height / 2, random(width), random(height));
+ 
+    if (mouseIsPressed)
+    {
+      background(0);
+      
+    }
 }
